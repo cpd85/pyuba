34d532db358c46f2330acb325b870b9a19feffb9
cdonaghy <cdonaghy@cdonaghys-MacBook-Pro.local>
2014-06-09 21:55:03 -0700

    basic functionality

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ This is going to be my attempt to beat python's internal list implementation... 
 
 
 I'm going to start with just a UBA, then probably try and make the buffer circular.
+
+
+OK so for now, i'm compiling at command line with :
+
+gcc -shared -I/usr/include/python2.7/ -lpython2.7 -o pyuba.so pyuba.c
+
+Haven't run any tests yet, will tomorrow
---
+++ pyuba.c
@@ -0,0 +1,93 @@
+#include <Python/Python.h>
+
+typedef struct {
+  int *array; /*For future circular buffer use*/
+  int *start;
+  int *end;
+  int numitems;
+  int size;
+} pyuba_struct;
+
+
+static PyObject *
+pyuba_remove(PyObject *self, PyObject *args)
+{
+  int index, i;
+  unsigned long structptr;
+  pyuba_struct *pyuba_str;
+  if (PyArg_ParseTuple(args, "li", &structptr, &index) == 0) {
+    return NULL;
+  }
+  pyuba_str = (pyuba_struct *) structptr;
+  for (i = index; i < (pyuba_str->numitems -1); i++){
+    pyuba_str->array[i] = pyuba_str->array[i + 1];
+  }
+  pyuba_str->numitems--;
+  pyuba_str->end--;
+  return Py_None;
+}
+
+static PyObject *
+pyuba_insert(PyObject *self, PyObject *args)
+{
+  int elem, index;
+  unsigned long structptr;
+  int *newarray;
+  pyuba_struct *pyuba_str;
+  if (PyArg_ParseTuple(args, "lii", &structptr, &index, &elem) == 0) {
+    return NULL;
+  }
+  pyuba_str = (pyuba_struct *) structptr;
+  if (++(pyuba_str->numitems) == pyuba_str->size){
+    /* We need to double the array size before insertion */
+    newarray = malloc(2 * sizeof(int) * (pyuba_str->numitems));
+    memcpy(newarray, pyuba_str->array, (pyuba_str->numitems * sizeof(int)));
+    pyuba_str->array = newarray;
+    pyuba_str->start = newarray;
+    pyuba_str->end = newarray + pyuba_str->numitems;
+    pyuba_str->size *= 2;
+  } else {
+    int i;
+    int *start = pyuba_str->start;
+    for (i = (pyuba_str->numitems - 1); i > (index); i--){
+      pyuba_str->start[i + 1] = pyuba_str->start[i];
+    }
+    pyuba_str->start[index] = elem;
+    pyuba_str->end++;
+  }
+  return Py_None;
+}
+
+static PyObject *
+new_pyuba_method(PyObject *self, PyObject *args)
+{
+  int nitems;
+
+  if (PyArg_ParseTuple(args, "i", &nitems) == 0) {
+    return NULL;
+  }
+
+  /* Prepare pyuba structure */
+  pyuba_struct mystruct;
+  mystruct.array = malloc(sizeof(int) * 2 * nitems);
+  mystruct.size = 2 * nitems;
+  mystruct.numitems = nitems;
+  mystruct.start = mystruct.array;
+  mystruct.end = mystruct.end;
+  
+
+  return Py_BuildValue("i", (long) &mystruct);
+}
+
+static PyMethodDef pyuba_methods[] = {
+  {"insert", pyuba_insert, METH_VARARGS},
+  {"remove", pyuba_remove, METH_VARARGS},
+  {"new_pyuba", new_pyuba_method, METH_VARARGS},
+  {NULL, NULL}
+};
+
+void
+initpyuba()
+{
+  (void) Py_InitModule("pyuba", pyuba_methods);
+}
